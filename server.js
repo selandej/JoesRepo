130a6ec68689c5888c693aedeff8092510c2595e
selandej <josephoselander@yahoo.com>
2014-07-09 21:56:10 -0700

    Joe's First Commit

---
+++ server.js
@@ -0,0 +1,72 @@
+var restify = require('restify'),
+fs = require('fs'),
+url = require('url'),
+path = require('path');
+
+var server = restify.createServer();
+
+var port = 1337;
+
+loadsite = function (request, response, next)
+{
+console.log('loadingsite');
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+    console.log(request);
+    console.log(response);
+
+  if(request.url.toLowerCase().indexOf("/controllers/") > -1)
+	filename = "";
+  if(request.url.toLowerCase().indexOf("/appServer/") > -1)
+	filename = "";
+  if(request.url.toLowerCase().indexOf("/utilities/") > -1)
+	filename = "";
+
+  path.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n" + filename );
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+	console.log(filename);
+	fs.readFile(filename, "binary", function(err, file) {
+		if(err) {        
+			response.writeHead(500, {"Content-Type": "text/plain"});
+			response.write(err + "\n");
+			response.end();
+			return;
+		}
+		var responseType = "";	  
+		//fix specifically for contenttype in css, if not included IE9 it doesn't render for security purposes 
+		switch(path.extname(filename))
+		{
+		case ".html":
+			responseType = "text/html";
+			break;
+		case ".js":
+			responseType = "text/javascript";
+			break;
+		case ".css":
+			responseType = "text/css";
+			break;	
+		default:
+			//nada
+			break;
+		}
+		console.log(responseType);
+		responseType !== "" ? response.writeHead(200, {"Content-Type": responseType}) : false;
+		response.write(file, "binary");
+		response.end();
+	});
+  });
+}
+
+server.get('/.*', loadsite);
+
+server.listen(port, function() { console.log('%s listening at %s', server.name, server.url); });
